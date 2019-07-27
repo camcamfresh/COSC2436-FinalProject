999f54a0950da856267b035505041c0524404945
cameron <email@email.com>
2019-07-27 13:57:02 -0500

    Last edits/touch-ups.

--- graph.h
+++ graph.h
@@ -23,7 +23,8 @@ private:
 	bool symmetric();
 	bool fileExist(std::string);
 public:
-	//input number of vertices, pointer to square 2D int array, & bool to output vertices alphabetically (true) instead of numerically (false).
+	//input number of vertices, pointer to square 2D int array (aka the name of the array),
+	//    & bool to output vertices alphabetically (true) instead of numerically (false).
 	//no checks on the 2d matrix, disconnected edges are must be set to -1, unweighted edges must be set to 0 and weighted edges must be set to their respective integer weight.
 	Graph(int, void *, bool); 
 	~Graph();
